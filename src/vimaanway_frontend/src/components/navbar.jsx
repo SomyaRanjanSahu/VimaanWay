cfa4612706b929b6e274d76e0fec2c2ea9621bed
SomyaRanjanSahu <somya@Somya>
2023-10-26 18:46:06 +0530

    Updated readme

--- src/vimaanway_frontend/src/components/navbar.jsx
+++ src/vimaanway_frontend/src/components/navbar.jsx
@@ -4,13 +4,13 @@ import '../styles/navbar.css';
 function Navbar() {
   return (
     <nav className="navbar">
-      <div className="logo">Vimaan<span className="title-span">Way</span></div>
+      <a href="/" className="vimaanway"><div className="logo">Vimaan<span className="title-span">Way</span></div></a>      
       <ul className="nav-links">
         <li><a href="/travelGuide">Travel guide</a></li>
         <span className="partition">|</span>
         <li><a href="#">Enquire</a></li>
       </ul>
-      <button className="nav-button">Book ticket</button>
+      <button className="nav-button">Get ticket</button>
     </nav>
   );
 }
