01f911a1412919335e3cedffb714bef0d892939e
SomyaRanjanSahu <somya@Somya>
2023-10-26 15:43:16 +0530

    Managed routing

--- src/vimaanway_frontend/src/App.jsx
+++ src/vimaanway_frontend/src/App.jsx
@@ -8,9 +8,9 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* <Route path="/" element={<Home />} /> */}
-        <Route path="/search" element={<Search />} />
         <Route path="/" element={<Home />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/travelGuide" element={<TravelGuide />} />
       </Routes>
     </BrowserRouter>
   );
--- src/vimaanway_frontend/src/pages/travelGuide.jsx
+++ src/vimaanway_frontend/src/pages/travelGuide.jsx
@@ -25,44 +25,44 @@ function Guide() {
             <section class="carousel">
                 <div class="carousel__container">
                     <div class="carousel-item">
-                        <img class="carousel-item__img" src="https://pujacraft.com/cdn/shop/articles/Badrinath-Temple.jpg?v=1687449369" alt="people" />
+                        <img class="carousel-item__img" src="badrinath.jpg" alt="people" />
                         <div class="carousel-item__details">
                             <h5 class="carousel-item__details--title">BADRINATH</h5>
                         </div>
                     </div>
                     <div class="carousel-item">
-                        <img class="carousel-item__img" src="https://trisoj.com/travel-guide/wp-content/uploads/2022/11/Things-to-do-in-Nainital.png" alt="people" />
+                        <img class="carousel-item__img" src="nainital.png" alt="people" />
                         <div class="carousel-item__details">
                             <h5 class="carousel-item__details--title">NAINITAL</h5>
                         </div>
                     </div>
                     <div class="carousel-item">
-                        <img class="carousel-item__img" src="https://static.toiimg.com/photo/86290693.cms" alt="people" />
+                        <img class="carousel-item__img" src="mussorie.jpg" alt="people" />
                         <div class="carousel-item__details">
                             <h5 class="carousel-item__details--title">MUSSOORIE </h5>
                         </div>
                     </div>
                     <div class="carousel-item">
-                        <img class="carousel-item__img" src="https://media1.thrillophilia.com/filestore/cex97dhqa08x9ycdmcuiyh5fdlsr_1587991355_1.Khajjiar-Mini-Switzerland-of-India-3.jpg" alt="people" />
+                        <img class="carousel-item__img" src="dalhousie.jpg" alt="people" />
                         <div class="carousel-item__details">
                             <h5 class="carousel-item__details--title">DALHOUSIE</h5>
                         </div>
                     </div>
                     <div class="carousel-item">
-                        <img class="carousel-item__img" src="https://renokadventures.com/image/kullu.webp" alt="people" />
+                        <img class="carousel-item__img" src="kullu.jpg" alt="people" />
                         <div class="carousel-item__details">
                             <h5 class="carousel-item__details--title">KULLU</h5>
                         </div>
                     </div>
                     <div class="carousel-item">
-                        <img class="carousel-item__img" src="https://cf.bstatic.com/xdata/images/hotel/max1024x768/292138307.jpg?k=eb10c5a84dec52a944c9724bef9aa37b772fcb6205a75bffb838ebf734c9176f&o=&hp=1" alt="people" />
+                        <img class="carousel-item__img" src="shimla.jpg" alt="people" />
                         <div class="carousel-item__details">
                             <h5 class="carousel-item__details--title">SHIMLA</h5>
                         </div>
                     </div>
 
                     <div class="carousel-item">
-                        <img class="carousel-item__img" src="https://cf.bstatic.com/xdata/images/hotel/max1024x768/424037036.jpg?k=bbf192a709e0e698e17d8ab81cd9c5200d1c99c9333c0720228e14ba526e4501&o=&hp=1" alt="people" />
+                        <img class="carousel-item__img" src="manali.jpg" alt="people" />
                         <div class="carousel-item__details">
                             <h5 class="carousel-item__details--title">MANALI</h5>
                         </div>
@@ -75,44 +75,44 @@ function Guide() {
             <section class="carousel">
                 <div class="carousel__container">
                     <div class="carousel-item">
-                        <img class="carousel-item__img" src="https://www.tourmyindia.com/states/goa/image/sinquerim-beach-banner.webp" alt="people" />
+                        <img class="carousel-item__img" src="Sinquerim.jpg" alt="people" />
                         <div class="carousel-item__details">
                             <h5 class="carousel-item__details--title">Sinquerim Beach Goa</h5>
                         </div>
                     </div>
                     <div class="carousel-item">
-                        <img class="carousel-item__img" src="https://www.tourmyindia.com/states/goa/image/turtle-beach-banner.webp" alt="people" />
+                        <img class="carousel-item__img" src="turtle.jpg" alt="people" />
                         <div class="carousel-item__details">
                             <h5 class="carousel-item__details--title">Turtle Beach Goa</h5>
                         </div>
                     </div>
                     <div class="carousel-item">
-                        <img class="carousel-item__img" src="https://www.tourmyindia.com/states/goa/image/anjuna-beach-banner.webp" alt="people" />
+                        <img class="carousel-item__img" src="anjuna.jpg" alt="people" />
                         <div class="carousel-item__details">
                             <h5 class="carousel-item__details--title">Anjuna Beach Goa</h5>
                         </div>
                     </div>
                     <div class="carousel-item">
-                        <img class="carousel-item__img" src="https://www.tourmyindia.com/states/goa/image/bogmalo-beach-banner.webp" alt="people" />
+                        <img class="carousel-item__img" src="bogmalo.jpg" alt="people" />
                         <div class="carousel-item__details">
                             <h5 class="carousel-item__details--title">Bogmalo Beach Goa</h5>
                         </div>
                     </div>
                     <div class="carousel-item">
-                        <img class="carousel-item__img" src="https://media2.thrillophilia.com/images/photos/000/376/892/original/1640608257_baga_beach.jpg?w=753&h=450&dpr=1.0" alt="people" />
+                        <img class="carousel-item__img" src="baga.jpg" alt="people" />
                         <div class="carousel-item__details">
                             <h5 class="carousel-item__details--title">Baga Beach</h5>
                         </div>
                     </div>
                     <div class="carousel-item">
-                        <img class="carousel-item__img" src="https://media2.thrillophilia.com/images/photos/000/376/896/original/1640608655_Butterfly_Beach.jpg?w=753&h=450&dpr=1.0" alt="people" />
+                        <img class="carousel-item__img" src="butterfly.jpeg" alt="people" />
                         <div class="carousel-item__details">
                             <h5 class="carousel-item__details--title">Butterfly Beach</h5>
                         </div>
                     </div>
 
                     <div class="carousel-item">
-                        <img class="carousel-item__img" src="https://media2.thrillophilia.com/images/photos/000/376/900/original/1640608984_elephant_beach.jpg?w=753&h=450&dpr=1.0" alt="people" />
+                        <img class="carousel-item__img" src="elephanta.jpg" alt="people" />
                         <div class="carousel-item__details">
                             <h5 class="carousel-item__details--title">Elephant Beach</h5>
                         </div>
