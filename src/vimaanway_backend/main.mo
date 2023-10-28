9d6aa946f231126aa46c5532a44f142a9e08c7aa
SomyaRanjanSahu <somya@Somya>
2023-10-29 02:06:43 +0530

    Debugged search form

--- src/vimaanway_backend/main.mo
+++ src/vimaanway_backend/main.mo
@@ -70,21 +70,6 @@ actor {
 
   // Flight arrays to store flight details for different cities
 
-  let flightArray : [Text] = [
-    "IndiGo (6E-1248) | 08:00 - 10:30 | --> ₹3,500",
-    "Air India (AI626) | 09:30 - 11:45 | --> ₹4,200",
-    "SpiceJet (SG2341) | 13:15 - 15:30 | --> ₹3,800",
-    "AirAsia India (I59613) | 07:45 - 09:55 | --> ₹3,200",
-    "Vistara (UK929) | 10:30 - 12:45 | --> ₹4,800",
-    "Alliance Air (AA5412) | 11:00 - 13:15 | --> ₹4,000",
-    "Saudia (SU985) | 12:30 - 15:00 | --> ₹5,500",
-    "Qatar Airways (QA-1965) | 08:15 - 10:45 | --> ₹6,200",
-    "Air Arabia (AI582) | 10:15 - 12:30 | --> ₹4,600",
-    "Etihad Airways (EA5697) | 11:30 - 13:45 | --> ₹5,800",
-    "Lufthansa (L2514) | 09:45 - 12:00 | --> ₹7,000",
-    "Emirates (ER1110) | 13:00 - 15:30 | --> ₹6,800",
-  ];
-
   let mumbaiFlights : [Text] = [
     "Air India (AI626) | 09:30 - 11:45 | --> ₹4,200",
     "SpiceJet (SG2341) | 13:15 - 15:30 | --> ₹3,800",
@@ -100,7 +85,7 @@ actor {
     "Lufthansa (L2514) | 09:45 - 12:00 | --> ₹7,000",
   ];
 
-  let odishaFlights : [Text] = [
+  let bhubaneswarFlights : [Text] = [
     "IndiGo (6E-1248) | 08:00 - 10:30 | --> ₹3,500",
     "Air India (AI626) | 09:30 - 11:45 | --> ₹4,200",
     "SpiceJet (SG2341) | 13:15 - 15:30 | --> ₹3,800",
@@ -131,15 +116,25 @@ actor {
     "Lufthansa (L2514) | 09:45 - 12:00 | --> ₹7,000",
   ];
 
+  let hyderabadFlights : [Text] = [
+    "AirAsia India (I59613) | 07:45 - 09:55 | --> ₹3,200",
+    "Vistara (UK929) | 10:30 - 12:45 | --> ₹4,800",
+    "Lufthansa (L2514) | 09:45 - 12:00 | --> ₹7,000",
+    "Emirates (ER1110) | 13:00 - 15:30 | --> ₹6,800",
+    "Air India (AI626) | 09:30 - 11:45 | --> ₹4,200",
+    "SpiceJet (SG2341) | 13:15 - 15:30 | --> ₹3,800",
+  ];
+
   public query func getFlights() : async [Text] {
     switch (origin) {
       case ("Mumbai") { return mumbaiFlights };
       case ("Delhi") { return delhiFlights };
-      case ("Odisha") { return odishaFlights };
+      case ("Bhubaneswar") { return bhubaneswarFlights };
       case ("Kolkata") { return kolkataFlights };
       case ("Chennai") { return chennaiFlights };
       case ("Bangalore") { return bangaloreFlights };
-      case (_) { return flightArray };
+      case ("Hyderabad") { return hyderabadFlights };
+      case (_) { return [] };
     };
   };
 
--- src/vimaanway_frontend/src/components/flightSearch.jsx
+++ src/vimaanway_frontend/src/components/flightSearch.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../styles/flightSearch.css';
 import { useNavigate } from 'react-router-dom';
 import { vimaanway_backend } from '../../../declarations/vimaanway_backend';
@@ -10,10 +10,46 @@ const FlightSearchForm = () => {
     const [origin, setOrigin] = useState('');
     const [destination, setDestination] = useState('');
     const [departDate, setDepartDate] = useState(today);
-    const [returnDate, setReturnDate] = useState('');
+    const [returnDate, setReturnDate] = useState(today);
     const [travellers, setTravelers] = useState(1);
     const [travelCategory, setTravelCategory] = useState('');
     const [fareTypes, setFareTypes] = useState('');
+
+    // Origin dropdown suggestions
+    const [originSuggestions, setOriginSuggestions] = useState([]);
+    const [isOriginDropdownOpen, setOriginDropdownOpen] = useState(false);
+
+    // Destination dropdown suggestions
+    const [destinationSuggestions, setDestinationSuggestions] = useState([]);
+    const [isDestinationDropdownOpen, setDestinationDropdownOpen] = useState(false);
+
+    // No origin airport found
+    const [noOriginAirFn, setNoOriginAirFn] = useState(false);
+
+    // No destination airport found
+    const [noDestAirFn, setNoDestAirFn] = useState(false);
+
+    useEffect(() => {
+        const airports = ['Bangalore', 'Bhubaneswar', 'Chennai', 'Delhi', 'Hyderabad', 'Kolkata', 'Mumbai'];
+
+        const filteredOriginSuggestions = airports.filter(airports => airports.toLowerCase().includes(origin.toLowerCase()));
+        const filteredDestinationSuggestions = airports.filter(airports => airports.toLowerCase().includes(destination.toLowerCase()));
+
+        if (filteredOriginSuggestions.length === 0) {
+            setNoOriginAirFn(true);
+        } else {
+            setNoOriginAirFn(false);
+        }
+
+        if (filteredDestinationSuggestions.length === 0) {
+            setNoDestAirFn(true);
+        } else {
+            setNoDestAirFn(false);
+        }
+        setOriginSuggestions(filteredOriginSuggestions);
+        setDestinationSuggestions(filteredDestinationSuggestions);
+
+    }, [origin, destination]);
 
     const navigate = useNavigate();
 
@@ -34,9 +70,13 @@ const FlightSearchForm = () => {
         await Promise.all(promises);
 
         // Checking if the required fields were filled
-        if (origin && destination && fareTypes) {
+        if (origin == destination) {
+            alert("Origin and destination cannot be the same!");
+        }
+        else if (origin && destination && fareTypes && !noOriginAirFn && !noDestAirFn) {
             navigate("./search");
-        } else {
+        }
+        else {
             alert("Please fill all the required fields!");
         }
     };
@@ -76,21 +116,85 @@ const FlightSearchForm = () => {
                         <tr>
                             <td className="left-data">
                                 <label htmlFor="" className="required-field">From :</label>
-                                <input
-                                    type="text"
-                                    placeholder="Origin"
-                                    value={origin}
-                                    onChange={(e) => setOrigin(e.target.value)}
-                                />
+                                <div style={{ position: 'relative' }}>
+                                    <input
+                                        type="text"
+                                        placeholder="Origin"
+                                        value={origin}
+                                        onChange={(e) => {
+                                            setOrigin(e.target.value);
+                                            setOriginDropdownOpen(true);
+                                        }}
+                                        onFocus={() => setOriginDropdownOpen(true)}
+                                    />
+                                    {origin && isOriginDropdownOpen && (originSuggestions.length > 0 || noOriginAirFn) && (
+                                        <div style={{ position: 'absolute', top: '100%', left: 0, zIndex: 1, backgroundColor: '#f7efeb', border: '2px solid #333', width: '200px', borderRadius: '10px' }}>
+                                            {noOriginAirFn ? (<p style={{ padding: '8px' }} >No Airport Found </p>) : (
+                                                <ul style={{ listStyle: 'none', padding: 0 }}>
+                                                    {originSuggestions.map((item, index) => (
+                                                        <li
+                                                            key={index}
+                                                            onClick={() => {
+                                                                setOrigin(item);
+                                                                setOriginDropdownOpen(false);
+                                                            }}
+                                                            style={{ padding: '8px', cursor: 'pointer', }}
+                                                            onMouseEnter={(e) => {
+                                                                e.target.style.backgroundColor = '#d6c1b8'; // Change to your desired hover color
+                                                            }}
+                                                            onMouseLeave={(e) => {
+                                                                e.target.style.backgroundColor = '#f7efeb'; // Revert to the original color on mouse leave
+                                                            }}
+                                                        >
+                                                            {item}
+                                                        </li>
+                                                    ))}
+                                                </ul>
+                                            )}
+                                        </div>
+                                    )}
+                                </div>
                             </td>
                             <td className="left-data">
                                 <label htmlFor="" className="required-field">To :</label>
-                                <input
-                                    type="text"
-                                    placeholder="Destination"
-                                    value={destination}
-                                    onChange={(e) => setDestination(e.target.value)}
-                                />
+                                <div style={{ position: 'relative' }}>
+                                    <input
+                                        type="text"
+                                        placeholder="Destination"
+                                        value={destination}
+                                        onChange={(e) => {
+                                            setDestination(e.target.value);
+                                            setDestinationDropdownOpen(true);
+                                        }}
+                                        onFocus={() => setDestinationDropdownOpen(true)}
+                                    />
+                                    {destination && isDestinationDropdownOpen && (destinationSuggestions.length > 0 || noDestAirFn) && (
+                                        <div style={{ position: 'absolute', top: '100%', left: 0, zIndex: 1, backgroundColor: '#f7efeb', border: '2px solid #333', width: '200px', borderRadius: '10px' }}>
+                                            {noDestAirFn ? (<p style={{ padding: '8px' }} >No Airport Found </p>) : (
+                                                <ul style={{ listStyle: 'none', padding: 0 }}>
+                                                    {destinationSuggestions.map((item, index) => (
+                                                        <li
+                                                            key={index}
+                                                            onClick={() => {
+                                                                setDestination(item);
+                                                                setDestinationDropdownOpen(false);
+                                                            }}
+                                                            style={{ padding: '8px', cursor: 'pointer', }}
+                                                            onMouseEnter={(e) => {
+                                                                e.target.style.backgroundColor = '#d6c1b8'; // Change to your desired hover color
+                                                            }}
+                                                            onMouseLeave={(e) => {
+                                                                e.target.style.backgroundColor = '#f7efeb'; // Revert to the original color on mouse leave
+                                                            }}
+                                                        >
+                                                            {item}
+                                                        </li>
+                                                    ))}
+                                                </ul>
+                                            )}
+                                        </div>
+                                    )}
+                                </div>
                             </td>
                         </tr>
                         <tr>
